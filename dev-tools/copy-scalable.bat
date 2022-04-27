66abb3d62760f5403efbed874fdcc74ec2a5670f
Luis Eduardo Ribeiro Guerra <luiseduardoribeiroguerra@gmail.com>
2022-04-27 14:14:23 -0300

    copy tool

---
+++ dev-tools/copy-scalable.bat
@@ -0,0 +1,13 @@
+SET folder16="../Darwin/16x16"
+SET folder22="../Darwin/22x22"
+SET folder24="../Darwin/24x24"
+SET folder32="../Darwin/32x32"
+SET folder48="../Darwin/48x48"
+SET folder64="../Darwin/64x64"
+xcopy /i /e "scalable" %folder16%
+xcopy /i /e "scalable" %folder22%
+xcopy /i /e "scalable" %folder24%
+xcopy /i /e "scalable" %folder32%
+xcopy /i /e "scalable" %folder48%
+xcopy /i /e "scalable" %folder64%
+pause
