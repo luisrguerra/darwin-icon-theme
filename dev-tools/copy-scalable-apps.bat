9f60b7e9fbbf0b0875f5e347af3fc8e642fb78d7
Luis Eduardo Ribeiro Guerra <luiseduardoribeiroguerra@gmail.com>
2022-04-27 17:24:04 -0300

    rythmbox icon improvement

--- dev-tools/copy-scalable-apps.bat
+++ dev-tools/copy-scalable-apps.bat
@@ -9,5 +9,6 @@ xcopy /i /e /y "scalable" %folder22%
 xcopy /i /e /y "scalable" %folder24%
 xcopy /i /e /y "scalable" %folder32%
 xcopy /i /e /y "scalable" %folder48%
-xcopy /i /e /y "scalable" %folder64%
+xcopy /i /e /y "scalable/*" %folder64%
+del "scalable" /s /q
 pause
