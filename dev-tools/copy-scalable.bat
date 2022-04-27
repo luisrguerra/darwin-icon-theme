9565fa253ad25849082b730cfb2aededf9d91594
Luis Eduardo Ribeiro Guerra <luiseduardoribeiroguerra@gmail.com>
2022-04-27 15:12:22 -0300

    Extension icon

--- dev-tools/copy-scalable.bat
+++ dev-tools/copy-scalable.bat
@@ -1,9 +1,9 @@
-SET folder16="../Darwin/16x16"
-SET folder22="../Darwin/22x22"
-SET folder24="../Darwin/24x24"
-SET folder32="../Darwin/32x32"
-SET folder48="../Darwin/48x48"
-SET folder64="../Darwin/64x64"
+SET folder16="../Darwin/16x16/apps"
+SET folder22="../Darwin/22x22/apps"
+SET folder24="../Darwin/24x24/apps"
+SET folder32="../Darwin/32x32/apps"
+SET folder48="../Darwin/48x48/apps"
+SET folder64="../Darwin/64x64/apps"
 xcopy /i /e "scalable" %folder16%
 xcopy /i /e "scalable" %folder22%
 xcopy /i /e "scalable" %folder24%
